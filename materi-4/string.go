322cc59cca39393fc1abaafa438dc9a6231db92b
Prince Yazna <imtiyaznajih8@gmail.com>
2026-02-10 09:05:18 +0700

    fix: convert materi-4 from submodule to normal folder

---
+++ materi-4/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Prince")
+	fmt.Println("Prince Sal")
+	fmt.Println("Prince Sal Yazna")
+
+	fmt.Println(len("Prince"))
+	fmt.Println("Prince Sal"[0])
+	fmt.Println("Prince Sal Yazna"[1])
+}
